d02fa93e351ef67aebc836a4eeff3a824e228c96
adamliu84 <adam.liu84@gmail.com>
2016-12-12 21:34:58 +0800

    Refactoring import library

--- PageFetcher.hs
+++ PageFetcher.hs
@@ -1,24 +1,17 @@
-import System.Environment
-import Network.HTTP.Conduit
-import Network.HTTP
-import qualified Data.ByteString.Lazy as LB (unpack)
+import System.Environment (getArgs)
+import Network.HTTP.Conduit (simpleHttp)
 import Data.Char (chr)
-import Data.Time
-import Data.Time.Format
-import System.IO.Unsafe
-import Data.List.Split
-import Data.String.Utils (replace)
-import Data.Time.Clock
-import Data.Time.LocalTime
-import Control.Monad
-import GHC.IO.Encoding
+import Control.Monad (when)
+import GHC.IO.Encoding (setLocaleEncoding, utf8)
+import qualified Data.ByteString.Lazy as LB (unpack)
+import qualified Data.Time as TI
 
 downloadPage ::  String -> IO [String]
-downloadPage url = do 
+downloadPage url = do
                   return.lines.bsToStr =<< simpleHttp url
                 where bsToStr = map (chr . fromEnum).LB.unpack
-                
-timelogfilename :: String     
+
+timelogfilename :: String
 timelogfilename = "time.log"
 
 writetimelog :: String -> IO()
@@ -28,19 +21,19 @@ writetimelog hiturl = do
 
 formCurrentTime:: IO String
 formCurrentTime = do
-    now <- getCurrentTime
-    timezone <- getCurrentTimeZone
-    let szCurDate = yymmddCsv.toGregorian.utctDay $ now
-        szCurTime = hhmmss.localTimeOfDay $ utcToLocalTime timezone now
+    now <- TI.getCurrentTime
+    timezone <- TI.getCurrentTimeZone
+    let szCurDate = yymmddCsv.TI.toGregorian.TI.utctDay $ now
+        szCurTime = hhmmss.TI.localTimeOfDay $ TI.utcToLocalTime timezone now
     return (szCurDate++"_"++szCurTime)
     where yymmddCsv (y,m,d) =  concat [show y, pad2zero m, pad2zero d]
-          hhmmss (TimeOfDay h m s) = concat[pad2zero h, pad2zero m, take 2 $ pad2zero s]
+          hhmmss (TI.TimeOfDay h m s) = concat[pad2zero h, pad2zero m, take 2 $ pad2zero s]
           pad2zero x = if x < 10 then
                         reverse.show $ x * 10
                        else
-                        show x     
-                
-htmllogfilename :: String                
+                        show x
+
+htmllogfilename :: String
 htmllogfilename = "html.log"
 
 writehtmllog :: String -> IO()
@@ -57,19 +50,19 @@ helptext = "Usage: PageFetcher target_page [-l] [-p] \
             \Sample:\n \
             \\tPageFetcher.exe http://www.nosuchpage.com -h -l -p\
             \\n\n"
-             
+
 main :: IO()
 main = do
     setLocaleEncoding utf8
     args <- getArgs
-    if (1 <= length args) then 
+    if (1 <= length args) then
         do pagehtml <- downloadPage (args!!0)
             -- Check if required to print help
            when ("-h" `elem` args) $ putStr $ helptext
            -- Check if required to log timestamp
-           when ("-l" `elem` args) $ writetimelog (args!!0)                       
+           when ("-l" `elem` args) $ writetimelog (args!!0)
            -- Check if requireed to log the output html
-           when ("-p" `elem` args) $ writehtmllog $ concat pagehtml           
+           when ("-p" `elem` args) $ writehtmllog $ concat pagehtml
     else
         do putStr $ helptext ++ "\n"
            error $ "Invalid arguments input"
